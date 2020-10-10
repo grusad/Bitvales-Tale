486d72f05b6c398cd912b271f169e74d87d7eb0d
grusad <gillberg.alexander@gmail.com>
2020-10-10 19:44:38 +0200

    new model, new animations

--- src/Player.gd
+++ src/Player.gd
@@ -7,6 +7,7 @@ const MAX_MOVEMENT_SPEED = 15
 onready var animation_tree = $AnimationTree
 
 var velocity = Vector3()
+var rolling = false
 
 func _physics_process(delta):
 	
@@ -21,7 +22,9 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		move_dir.x = 1
 	if Input.is_action_just_pressed("default_attack"):
-		animation_tree["parameters/punch/active"] = true
+		animation_tree["parameters/punch_attack/active"] = true
+	if Input.is_action_just_pressed("roll"):
+		animation_tree["parameters/roll/active"] = true
 		
 	move_dir = move_dir.normalized()
 	
@@ -43,4 +46,5 @@ func _physics_process(delta):
 	#lerp animations
 	var speed = horizontal_velocity.length() / MAX_MOVEMENT_SPEED
 	animation_tree["parameters/idle_move/blend_amount"] = speed
+
 	
