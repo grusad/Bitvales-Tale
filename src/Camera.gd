bbb8d6dba9916094154faf98580da4b43f17bec9
grusad <gillberg.alexander@gmail.com>
2021-04-04 21:36:40 +0200

    he

--- src/Camera.gd
+++ src/Camera.gd
@@ -2,11 +2,13 @@ extends Camera
 
 onready var player = get_parent().get_node("PlayerCharacter")
 
-
-
+	
 func _process(delta):
 	var offset = Vector3(0, 20, 14)
-	var target = player.global_transform.origin + offset
-	global_transform.origin = lerp(global_transform.origin, target, 0.2)
+	var target = player.global_transform.origin
+	global_transform.origin = lerp(global_transform.origin, target + offset, 0.2)
+	
+
+	
 	
 	
